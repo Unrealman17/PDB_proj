300e5bf158957b05248a358311e1a75d709acc87
var_vep@mail.ru <var_vep@mail.ru>
2023-02-23 09:27:05 +0000

    mount_storage_account refactoring

--- Notebooks/install_run.py
+++ Notebooks/install_run.py
@@ -1,27 +1,25 @@
 # Databricks notebook source
-from pathlib import Path
-path = 
-print(path)
-
-# COMMAND ----------
-
 # MAGIC %sh
-# MAGIC 
 # MAGIC pip install gemmi
 
 # COMMAND ----------
 
 import os
 import sys
+from pathlib import Path
 os.chdir('..')
 sys.path.append(os.getcwd())
 print(os.getcwd())
+
+# COMMAND ----------
+
 from pdb_helper import spark, read_config
 from installer import reinstall
 from fill_config_table import fill
 from configure import configure
 from download_file import download_unzip
 from main import main
+from mount_storage_account import mount_storage_account
 
 # COMMAND ----------
 
--- mount_storage_account.py
+++ mount_storage_account.py
@@ -1,15 +1,23 @@
 # Databricks notebook source
-dbutils.fs.mount(
-  source = 'wasbs://test@evarganovstorage.blob.core.windows.net',
-  mount_point= '/mnt/testblobstorage',
-  extra_configs= {'fs.azure.account.key.evarganovstorage.blob.core.windows.net':
-                      dbutils.secrets.get(scope = "MySecretScope", key = "ACCESS_KEY_STORAGE_ACCOUNT")}
-)
+from pdb_helper import read_config
 
-# COMMAND ----------
+def mount_storage_account():
+    storage_account_mount_point = read_config()["storage_account_mount_point"]
 
-#with open('//dbfs/mnt/testblobstorage/test.txt', 'w') as f:
-#    f.write('1234567890')
+    if storage_account_mount_point not in [m.mountPoint for m in dbutils.fs.mounts()]:
+        print(f'mounting {storage_account_mount_point} . . .')
+        dbutils.fs.mount(
+          source = 'wasbs://test@evarganovstorage.blob.core.windows.net',
+          mount_point= '/mnt/testblobstorage',
+          extra_configs= {'fs.azure.account.key.evarganovstorage.blob.core.windows.net':
+                              dbutils.secrets.get(scope = "MySecretScope", key = "ACCESS_KEY_STORAGE_ACCOUNT")}
+        )
+        print('Done!')
+    else:
+        print(f'{storage_account_mount_point} already mounted.')
+
+if __name__ == "__main__":
+    mount_storage_account()
 
 # COMMAND ----------
 
